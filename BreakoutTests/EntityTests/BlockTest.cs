851e3e76ce8ca6ffb6dcf3b728fb2e331f6075f8
magnusThorsen <dfc747@alumni.ku.dk>
2022-04-28 11:43:21 +0200

    All tests working

--- BreakoutTests/EntityTests/BlockTest.cs
+++ BreakoutTests/EntityTests/BlockTest.cs
@@ -3,45 +3,47 @@ using NUnit.Framework;
 using System;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
-using Galaga;
 using System.IO;
+using Breakout;
 using DIKUArcade.Events;
 using DIKUArcade;
 
 namespace BreakoutTests {
+
     [TestFixture]
     public class BlockTest {
-        private Blocks block;
+        private Block block;
         private Shape shape;
         private Image image;
 
         public BlockTest() {
             DIKUArcade.GUI.Window.CreateOpenGLContext();
-            shape = new Shape(new Vec2F(0.5f, 0.9f), new Vec2F(0.1f, 0.1f));
-            image = new Image(Path.Combine("Assets", "Images", "blue-block.png"));
-            block = new Blocks(shape, image);
+            shape = new DynamicShape(new Vec2F(0.5f, 0.5f), new Vec2F(1.0f/12, (1.0f/12)/3f));
+            image = new Image(@"../Breakout/Assets/Images/blue-block.png");
+            block = new Block(shape, image);
         }
 
         [SetUp]
         public void InitiateBlock() {
             DIKUArcade.GUI.Window.CreateOpenGLContext();
-            shape = new DynamicShape(new Vec2F(0.5f, 0.9f), new Vec2F(0.1f, 0.1f));
-            image = new Image(Path.Combine("Assets", "Images", "blue-block.png"));
-            block = new Blocks(shape, image);
+            shape = new DynamicShape(new Vec2F(0.5f, 0.5f), new Vec2F(1.0f/12, (1.0f/12)/3f));
+            image = new Image(@"../Breakout/Assets/Images/blue-block.png");
+            block = new Block(shape, image);
 
         }
 
         //Testing if a block´s health decreases as it should.
         [Test]
         public void TestDecHealth() {
-            block.DecHealth();
-            Assert.AreEqual(block.Health(), 0);
+            block.BlockHit();
+            Assert.AreEqual(block.GetHealth(), 0);
         }
 
         //Testing if a block is being destroyed (deleted) after having 0 or under in health.
         [Test]
         public void TestDeleteBlock() {
-            Assert.True(block, IsDeleted());
+            block.BlockHit();
+            Assert.IsTrue(block.IsDeleted() == true);
         }
     }
 }
