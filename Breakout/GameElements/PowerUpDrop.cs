e58b05ce77984d0b0cfe385e39d70400952c27bc
magnusThorsen <dfc747@alumni.ku.dk>
2022-05-25 13:44:32 +0200

    PowerUpDrop done

--- Breakout/GameElements/PowerUpDrop.cs
+++ Breakout/GameElements/PowerUpDrop.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
@@ -11,6 +13,9 @@ namespace Breakout {
         private static Vec2F extend; 
         public DynamicShape shape {get;}
         private Entity entity;
+        private int randNumber;
+        private Random rand; 
+        private PowerUps pwUp;
 
         public PowerUpDrop(DynamicShape shape, IBaseImage image) : base(shape, image) {
             this.shape = shape;
@@ -18,6 +23,9 @@ namespace Breakout {
             Yvelocity = 0.1f;
             shape.Direction = new Vec2F(0.0f,0.01f);
             extend = new Vec2F(0.04f, 0.04f);
+            rand = new Random(); 
+            randNumber = rand.Next(5);
+            pwUp = new PowerUps();
         }
 
         public void Render() {
@@ -29,8 +37,35 @@ namespace Breakout {
             shape.Move();
         }
 
+        public void RandomPower(int number) {
+            switch (number) {
+                case 0: 
+                    pwUp.LifePowerUp();
+                    entity.Image = new Image(Path.Combine("Assets", "Images", "LifePickUp.png"));
+                    break;
+                case 1: 
+                    pwUp.DoubleSpeedPowerUp();
+                    entity.Image = new Image(Path.Combine("Assets", "Images", "DoubleSpeedPowerUp.png"));
+                    break;
+                case 2: 
+                    pwUp.PlayerSpeedPowerUp();
+                    entity.Image = new Image(Path.Combine("Assets", "Images", "PlayerSpeedPowerUp.png"));
+                    break;
+                case 3: 
+                    pwUp.MoreTimePowerUp();
+                    entity.Image = new Image(Path.Combine("Assets", "Images", "hourglass.png"));
+                    break;
+                case 4: 
+                    pwUp.WidePowerUp();
+                    entity.Image = new Image(Path.Combine("Assets", "Images", "WidePowerUp.png"));
+                    break;
+                default: break;
+            }
+        }
+
         public void Consume(Player player) {
             if (CollisionDetection.Aabb(shape, player.Shape).Collision) {
+                RandomPower(randNumber);
                 this.DeleteEntity();
             }
         }
